01091eacf6dd75259ce08fc228fc7a200ab459cc
Sol Boucher <slb1566@rit.edu>
2013-01-07 00:17:56 -0500

    Added those of Lane's unit tests that compile
    These do not yet run successfully...

---
+++ test/LocationTest.java
@@ -0,0 +1,83 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Unit test suite for <tt>Location</tt>.
+ * @author Lane Lawley <lxl5734@rit.edu>
+ */
+@RunWith(JUnit4.class)
+public class LocationTest {
+	@Test
+	public void testNormalConstruction() {
+		String[] nearby = {"Dave's", "Joe's", "Jack's"};
+
+		Location loc = new Location(
+			54321,
+			"New York",
+			nearby
+		);
+
+		Assert.assertTrue(loc.getZipCode() == 54321);
+		Assert.assertTrue(loc.getState().equals("New York"));
+		Assert.assertTrue(loc.getNearbyBusinesses()[1].equals("Joe's"));
+	}
+
+	@Test
+	public void testCopyConstruction() {
+		String[] nearby = {"Dave's", "Joe's", "Jack's"};
+
+		Location loc = new Location(14586, "New York", nearby);
+
+		Location locCopy = new Location(loc);
+
+		Assert.assertTrue(loc.equals(locCopy));
+		Assert.assertFalse(loc == locCopy);
+	}
+
+	@Test
+	public void testNegativeZipCode() {
+		boolean testFailed = false;
+
+		String[] nearby = {"Dave's", "Joe's", "Jack's"};
+
+		try {
+			Location t = new Location(-6, "New York", nearby);
+		} catch(IllegalArgumentException e) {
+			testFailed = true;
+		} finally {
+			Assert.assertTrue(testFailed);
+		}
+	}
+
+	@Test
+	public void testNoStateGiven() {
+		boolean testFailed = false;
+
+		String[] nearby = {"Dave's", "Joe's", "Jack's"};
+
+		try {
+			Location t = new Location(14586, null, nearby);
+		} catch(IllegalArgumentException e) {
+			testFailed = true;
+		} finally {
+			Assert.assertTrue(testFailed);
+		}
+	}
+
+	@Test
+	public void testNoNearbyBusinessesGiven() {
+		boolean testFailed = false;
+
+		try {
+			Location t = new Location(14586, "New York", null);
+		} catch(IllegalArgumentException e) {
+			testFailed = true;
+		} finally {
+			Assert.assertTrue(testFailed);
+		}
+	}
+}
