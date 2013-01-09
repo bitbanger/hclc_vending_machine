e4bc3c54381d7db3e326ce0b1ea5231fa7b1b974
Matthew Koontz <mjk3979@rit.edu>
2013-01-09 04:03:46 -0500

    Added getTransactionsByState and getTransactionsByZipCode to DatabaseLayer.

--- src/DatabaseLayer.java
+++ src/DatabaseLayer.java
@@ -747,6 +747,66 @@ public class DatabaseLayer
 	}
 
 	/**
+	 * Fetches all of the transactions that occurred at the given zip code
+	 * @param zipCode The zip code at which the desired transactions occurred.
+	 * @return A collection containing the transactions that occurred at the
+	 * given zip code.
+	 * @throws SQLException in case of a database error
+	 **/
+	public Collection<Transaction> getTransactionsByZipCode(int zipCode) throws SQLException
+	{
+		Collection<Transaction> transactions = new LinkedList<Transaction>();
+		Statement stmt = db.createStatement();
+		String query = "SELECT transactionId, timestamp, VMTransaction.machineId, customerId, productId, rowX, rowY FROM VMTransaction JOIN VendingMachine JOIN Location ON VMTransaction.machineId = VendingMachine.machineId AND VendingMachine.locationId = Location.locationId WHERE Location.zipCode=" + zipCode;
+		ResultSet results = stmt.executeQuery(query);
+		while (results.next())
+		{
+			int id = results.getInt(1);
+			GregorianCalendar time = new GregorianCalendar();
+			time.setTimeInMillis(results.getLong(2));
+			VendingMachine machine = getVendingMachineById(results.getInt(3));
+			Customer customer = getCustomerById(results.getInt(4));
+			FoodItem product = getFoodItemById(results.getInt(5));
+			Pair<Integer, Integer> row = new Pair<Integer, Integer>(results.getInt(6), results.getInt(7));
+			Transaction transaction = new Transaction(time, machine, customer, product, row);
+			transaction.setId(id);
+			transactions.add(transaction);
+		}
+		results.close();
+		return transactions;
+	}
+
+	/**
+	 * Fetches all of the transactions that occurred in the given state
+	 * @param state The state in which the desired transactions occurred.
+	 * @return A collection containing the transactions that occurred in the
+	 * given state.
+	 * @throws SQLException in case of a database error
+	 **/
+	public Collection<Transaction> getTransactionsByState(String state) throws SQLException
+	{
+		Collection<Transaction> transactions = new LinkedList<Transaction>();
+		Statement stmt = db.createStatement();
+		String query = "SELECT transactionId, timestamp, VMTransaction.machineId, customerId, productId, rowX, rowY FROM VMTransaction JOIN VendingMachine JOIN Location ON VMTransaction.machineId = VendingMachine.machineId AND VendingMachine.locationId = Location.locationId WHERE Location.state=\"" + state + "\"";
+		ResultSet results = stmt.executeQuery(query);
+		while (results.next())
+		{
+			int id = results.getInt(1);
+			GregorianCalendar time = new GregorianCalendar();
+			time.setTimeInMillis(results.getLong(2));
+			VendingMachine machine = getVendingMachineById(results.getInt(3));
+			Customer customer = getCustomerById(results.getInt(4));
+			FoodItem product = getFoodItemById(results.getInt(5));
+			Pair<Integer, Integer> row = new Pair<Integer, Integer>(results.getInt(6), results.getInt(7));
+			Transaction transaction = new Transaction(time, machine, customer, product, row);
+			transaction.setId(id);
+			transactions.add(transaction);
+		}
+		results.close();
+		return transactions;
+	}
+
+	/**
 	 * Fetches all of the transactions the given customer has made.
 	 * @param customer The customer that performed the transactions you desire.
 	 * @return A collection containing the transactions the given customer
