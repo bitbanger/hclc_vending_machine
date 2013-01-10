d2082a2ebff9a96e96e7f30152061bb49f331d5a
ksavvy <kms7341@rit.edu>
2013-01-09 21:54:50 -0500

    two new skeletons

---
+++ src/ManagerMachineManagementScreen.java
@@ -0,0 +1,79 @@
+/**
+ *
+ * @author Kyle Savarese
+ *
+ */
+
+public class ManagerMachineManagamentScreen {
+
+	/** the database */
+	private static DatabaseLayer db = DatabaseLayer.getInstance();
+
+	/** the machines */
+	private Collection<VendingMachine> storefronts;
+
+	/**
+	 * base constructor
+	 * @param vms the machines
+	 */
+	public ManagerMachineManagementScreen( Collection<VendingMachine> vms ) {
+		storefronts = vms;
+	}
+
+	/**
+	 * lists all machines
+	 * @return a collection of all machines
+	 */
+	public Collection<VendingMachine> listMachinessAll() {
+		return storefronts;
+	}
+
+	/**
+	 * lists machines by location
+	 * @return lists the machines
+	 */
+	public Collection<VendingMachine> listMachinesByLocation() {
+		
+	}
+
+	/**
+	 * adds a new machine
+	 * @param location the loc of the machine
+	 */
+	public void addMachine( Location location ) {
+
+	}
+
+	/**
+	 * deactivate a machine
+	 * @param id the id of the machine
+	 */
+	public void deactivateMachine( int id ) {
+
+	}
+
+	/**
+	 * reactivates a machine
+	 * @param id the id of the machine
+	 */
+	public void reactivateMachine( int id ) {
+
+	}
+	
+	/**
+	 * changes a machines location
+	 * @param id the machines is
+	 * @param location the new location
+	 * @return whether it succeeded
+	 */
+	public boolean changeMachineLocation( int id, Location location ) {
+
+	}
+
+	/**
+	 * exits the screen
+	 */
+	public void exit() {
+
+	}
+}
---
+++ src/ManagerStockedItemsScreen.java
@@ -0,0 +1,77 @@
+/**
+ *
+ * @author Kyle Savarese
+ *
+ */
+
+public class ManagerStockedItemsScreen {
+
+	/** the database */
+	private static DatabaseLayer db = DatabaseLayer.getInstance();
+
+	/** all the fooditems */
+	private ArrayList<FoodItem> stockpile;
+
+	/**
+ 	 * constructor
+	 */
+	public ManagerStockedItemsScreen( Collection<FoodItem> items ) {
+		stockpile = new ArrayList<FoodItem> ( items );
+	}
+
+	/**
+	 * lists the available items
+	 */
+	public Collection<FoodItem> listItems() {
+		return stockpile;
+	}
+	
+	/**
+	 * adds an item to the collection
+	 * @param name the name of the item
+	 * @param price the price in cents
+	 * @param freshLength the amount of time the item stays fresh
+ 	 */
+	public void addItem( String name, int price, long freshLength ) {
+		FoodItem next = new FoodItem( name, price, freshLength );
+		db.updateOrCreateFoodItem( next );
+		stockpile.add( next );
+	}
+	
+	/**
+	 * changes an item name
+	 * @param id the id of the item
+	 * @param name the new name
+	 * @return boolean whether it succeeded
+	 */
+	public boolean changeItemName( int id, String name ) {
+
+	}
+
+	/** 
+	 * change an item's price
+ 	 * @param id the id of the item
+	 * @param price the new price
+	 * @return boolean whether it succeeded
+	 */
+	public boolean changeItemPrice( int id, int price ) {
+
+	}
+
+	/**
+	 * change item length
+	 * @param id the id of the item
+	 * @param freshLength the new length
+	 * @return boolean whether it succeeded
+	 */
+	public boolean changeItemFreshLength( int id, long freshLength ) {
+
+	}
+
+	/** 
+	 * exit the screen
+	 */
+	public void exit() {
+
+	}
+}
