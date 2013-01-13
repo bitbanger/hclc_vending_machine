5be3d4eb99e4d674871e769a66261cecccb08983
Sol Boucher <slb1566@rit.edu>
2013-01-13 16:55:49 -0500

    Added to CustomerLoginScreen: listActiveMachines() and buildInstance()
    These static methods are for the view's use when it first starts up.

--- src/CustomerLoginScreen.java
+++ src/CustomerLoginScreen.java
@@ -1,4 +1,6 @@
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * 1/3/2012
@@ -62,6 +64,50 @@ public class CustomerLoginScreen {
 		catch(Exception databaseProblem)
 		{
 			ControllerExceptionHandler.registerConcern(ControllerExceptionHandler.Verbosity.WARN, databaseProblem);
+			return null;
+		}
+	}
+
+	/**
+	 * Fetches a list of active vending machines.
+	 * @return the machine instances, which are not to be modified (or <tt>null</tt> on error)
+	 */
+	public static Collection<VendingMachine> listActiveMachines()
+	{
+		try
+		{
+			Collection<VendingMachine> ourUniverse=db.getVendingMachinesAll();
+			
+			//only keep active machines:
+			Iterator<VendingMachine> trimmer=ourUniverse.iterator();
+			while(trimmer.hasNext())
+				if(!trimmer.next().isActive())
+					trimmer.remove();
+			
+			return ourUniverse;
+		}
+		catch(Exception uhOh)
+		{
+			ControllerExceptionHandler.registerConcern(ControllerExceptionHandler.Verbosity.WARN, uhOh);
+			
+			return null;
+		}
+	}
+
+	/**
+	 * Builder for use in the view.
+	 * @param id the ID of the <tt>VendingMachine</tt>
+	 * @return an instance representing the machine, or <tt>null</tt> in case of trouble
+	 */
+	public static CustomerLoginScreen buildInstance(int id)
+	{
+		try
+		{
+			return new CustomerLoginScreen(db.getVendingMachineById(id));
+		}
+		catch(Exception problem)
+		{
+			ControllerExceptionHandler.registerConcern(ControllerExceptionHandler.Verbosity.ERROR, problem);
 			return null;
 		}
 	}
