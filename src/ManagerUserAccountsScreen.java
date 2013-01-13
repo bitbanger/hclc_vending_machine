caf7d1769b6c415fd70426028e919c180f8636ce
ksavvy <kms7341@rit.edu>
2013-01-13 12:22:22 -0500

    created the unit test for ManagerUserAccountsScreen also modified the addCustomer and addManager methods to return the ids 	of the customers/managers added

--- src/ManagerUserAccountsScreen.java
+++ src/ManagerUserAccountsScreen.java
@@ -50,18 +50,21 @@ public class ManagerUserAccountsScreen {
 	 * add a new customer
  	 * @param initialBalance the inital balance
 	 * @param name the name of the customer
+	 * @return the id of the new customer
 	 */
-	public void addCustomer( String name, int initialBalance ) {
+	public int addCustomer( String name, int initialBalance ) {
 		try
 		{
 			Customer cust = new Customer( name, initialBalance );
 			db.updateOrCreateCustomer( cust );
 			customers.add( cust );
+			return cust.getId();
 		}
 		catch(Exception databaseProblem)
 		{
 			System.err.println("ERROR: Database problem encountered!");
 			System.err.println("     : Dump details ... "+databaseProblem);
+			return -1;
 		}
 	}
 
@@ -69,18 +72,21 @@ public class ManagerUserAccountsScreen {
 	 * add a new manager
  	 * @param startingPassword the initial password
 	 * @param name the name of the manager
+	 * @return int the id of the new manager
 	 */
-	public void addManager( String name, String startingPassword ) {
+	public int addManager( String name, String startingPassword ) {
 		try
 		{
 			Manager manny = new Manager( name, startingPassword );
 			db.updateOrCreateManager( manny );
 			managers.add( manny );
+			return manny.getId();
 		}
 		catch(Exception databaseProblem)
 		{
 			System.err.println("ERROR: Database problem encountered!");
 			System.err.println("     : Dump details ... "+databaseProblem);
+			return -1;
 		}
 	}
 
