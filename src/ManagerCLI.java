979304144d85c9ff40e83aec326e54c3695578c2
Matthew Koontz <mjk3979@rit.edu>
2013-01-14 01:42:33 -0500

    Implemented alterLayout in ManagerCLI.
    Also changed some visibility modifiers to private.

--- src/ManagerCLI.java
+++ src/ManagerCLI.java
@@ -10,7 +10,7 @@ public class ManagerCLI
 	/**
 	 * Handles manager login stuff
 	 **/
-	public static void login()
+	private static void login()
 	{
 		while (true)
 		{
@@ -42,7 +42,7 @@ public class ManagerCLI
 	/**
 	 * Handles manager home screen
 	 **/
-	public static void home(ManagerHomeScreen home)
+	private static void home(ManagerHomeScreen home)
 	{
 		while (true)
 		{
@@ -60,7 +60,7 @@ public class ManagerCLI
 					viewStats(viewStats);
 					break;
 				case 1:
-					preAlterLayout();
+					preAlterLayout(home);
 					break;
 				case 6:
 					return;
@@ -71,7 +71,7 @@ public class ManagerCLI
 	/**
 	 * Handles viewing the statistics
 	 **/
-	public static void viewStats(ManagerReportStatsScreen screen)
+	private static void viewStats(ManagerReportStatsScreen screen)
 	{
 		while (true)
 		{
@@ -96,19 +96,15 @@ public class ManagerCLI
 	/**
 	 * Displays the statistics for all vending machines
 	 **/
-	public static void viewStatsAll(ManagerReportStatsScreen screen)
+	private static void viewStatsAll(ManagerReportStatsScreen screen)
 	{
 		Collection<Transaction> sales = screen.listSalesAll();
 		System.out.println("\n\nAll Sales:");
 		CLIUtilities.printCollection(sales);
 	}
 
-	/**
-	 * Displays the statistics for a single vending machine
-	 **/
-	public static void viewStatsMachine(ManagerReportStatsScreen screen)
+	private static VendingMachine vmChooser(Collection<VendingMachine> machines)
 	{
-		Collection<VendingMachine> machines = screen.listMachines();
 		CLIUtilities.printCollection(machines);
 		VendingMachine selected = null;
 		while (selected == null)
@@ -130,6 +126,42 @@ public class ManagerCLI
 				}
 			}
 		}
+		return selected;
+	}
+
+	private static FoodItem foodItemChooser(Collection<FoodItem> items)
+	{
+		CLIUtilities.printCollection(items);
+		FoodItem selected = null;
+		while (selected == null)
+		{
+			int choice = CLIUtilities.promptInt("Input food item id");
+			for (FoodItem item : items)
+			{
+				try
+				{
+					if (choice == item.getId())
+					{
+						selected = item;
+						break;
+					}
+				}
+				catch (Exception wtf)
+				{
+					ControllerExceptionHandler.registerConcern(ControllerExceptionHandler.Verbosity.FATAL, wtf);
+				}
+			}
+		}
+		return selected;
+	}
+
+	/**
+	 * Displays the statistics for a single vending machine
+	 **/
+	private static void viewStatsMachine(ManagerReportStatsScreen screen)
+	{
+		Collection<VendingMachine> machines = screen.listMachines();
+		VendingMachine selected = vmChooser(machines);
 		Collection<Transaction> sales = screen.listMachineSales(selected);
 		System.out.printf("\n\nSales from vending machine %s:\n", selected.toString());
 		CLIUtilities.printCollection(sales);
@@ -138,17 +170,56 @@ public class ManagerCLI
 	/**
 	 * Handles selecting the vending machine to alter
 	 **/
-	public static void preAlterLayout()
+	private static void preAlterLayout(ManagerHomeScreen screen)
 	{
-
+		Collection<VendingMachine> machines = screen.displayVendingMachines();
+		VendingMachine selected = vmChooser(machines);
+		ManagerAlterLayoutScreen alterLayout = new ManagerAlterLayoutScreen(selected);
+		alterLayout(alterLayout);
 	}
 
 	/**
 	 * Handles altering a machine's layout
 	 **/
-	public static void alterLayout(ManagerAlterLayoutScreen alterLayout)
+	private static void alterLayout(ManagerAlterLayoutScreen alterLayout)
 	{
-
+		while (true)
+		{
+			int choice = CLIUtilities.option(
+				"Change another row",
+				"Commit changes",
+				"Exit");
+			switch (choice)
+			{
+				case 0:
+					FoodItem[][] layout = alterLayout.listRows();
+					CLIUtilities.printLayout(layout);
+					int x = -1;
+					int y = -1;
+					while (x < 0 || y < 0 || x >= layout.length || y >= layout[0].length)
+					{
+						x = CLIUtilities.promptInt("X Value:");
+						y = CLIUtilities.promptInt("Y Value:");
+					}
+					System.out.println(layout[y][x]);
+					Collection<FoodItem> items = alterLayout.listItems();
+					FoodItem item = foodItemChooser(items);
+					boolean success = alterLayout.queueRowChange(new Pair<Integer, Integer>(y,x), item);
+					if (success)
+						System.out.println("Change queued successfully.\nYou still need to commit the changes before the changes become permanent");
+					else
+						System.out.println("An error occurred while attempting to queue the change");
+					break;
+				case 1:
+					boolean commitSuccess = alterLayout.commitRowChanges();
+					if (commitSuccess)
+						System.out.println("Changes committed successfully");
+					else
+						System.out.println("An error occurred while attempting to commit the changes");
+				case 2:
+					return;
+			}
+		}
 	}
 
 	/**
