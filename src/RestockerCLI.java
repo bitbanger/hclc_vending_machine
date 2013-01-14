6be5123912e5f8b05d6e0557db398b64bcf837e7
piper <piperchester@gmail.com>
2013-01-13 22:13:51 -0500

    Added improved RestockerCLI

--- src/RestockerCLI.java
+++ src/RestockerCLI.java
@@ -1,3 +1,6 @@
+import java.util.Collection;
+import java.util.Scanner;
+
 /**
  * CLI for the Restocker's perspective.
  * Provides an entry point for the Restocker.
@@ -8,31 +11,14 @@ public class RestockerCLI {
 	/** Instantiating a new RestockerMachinePickerScreen*/
 	private static RestockerMachinePickerScreen restockerMachinePickerScreen = new RestockerMachinePickerScreen();
 
-	/** Instatntiating a new RestockerTaskListScreen */
-	private static RestockerTaskListScreen restockerTaskListScreen;
 
-	/**
-	 * Begins the program and intitates primary options for the Restocker.
-	 */
-	private static void begin() {
-		int optionSelect = CLIUtilities.option("I'm viewing the to-do list", "I'm selecting a machine");
-
-		switch(optionSelect) {
-			case 0:
-				listTasks(restockerTaskListScreen);
-				break;
-			case 1:
-				pickMachine(restockerMachinePickerScreen);
-				break;
-		}
-	}	
 	/** The entry point to the program.
 	 * 
 	 * @param args Arguments required to create String arrays in Java
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Restocker interface!\n---\n");
-		begin();
+		pickMachine(restockerMachinePickerScreen);
 	}
 
 	/** Allows the Restocker to view the list of tasks that needs to be performed.
@@ -42,12 +28,14 @@ public class RestockerCLI {
 	private static void listTasks(RestockerTaskListScreen restockerTaskListScreen){
 		String[] tasks;
 
-		System.out.println("This is the list of tasks that need to be performed.");
+		CLIUtilities.printTitle("List Tasks Screen");
 	
 		tasks = restockerTaskListScreen.assembleStockingList();
 
 		for (String task : tasks)
 			System.out.println(task);
+
+		System.out.println("========\nApplication closed.");
 	}
 	
 	/**
@@ -56,8 +44,24 @@ public class RestockerCLI {
 	 * @param restockerMachinePickerScreen The state of the RestockerMachinePickerScreen
 	 */
 	private static void pickMachine(RestockerMachinePickerScreen restockerMachinePickerScreen) {
-		System.out.println("Please enter the number of the machine that needs to be selected.");
+		Collection<VendingMachine> vms = RestockerMachinePickerScreen.listActiveMachines();
+
+		int idNumber;
+	
+		CLIUtilities.printTitle("Pick Machine Screen");
+		
+		CLIUtilities.printCollection(vms);
+
+			
+		RestockerTaskListScreen restockerTaskListScreen = null;
+
+		while (restockerTaskListScreen == null){
+			idNumber = CLIUtilities.promptInt("Please give ID number");
+
+			restockerTaskListScreen = restockerMachinePickerScreen.tryMachine(idNumber);
+		}
 		
 
+		listTasks(restockerTaskListScreen);		
 	}
 }
