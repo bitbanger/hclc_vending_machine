cd0d52345bb579d2d21e8095a10bcdcc5fe7a35f
Sol Boucher <slb1566@rit.edu>
2013-01-28 21:09:44 -0500

    Revert "fixed issues with okay rows"
    This reverts commit f46bc058de6c3ca1e4df4e67769b1a8e1151d458.
    Why, oh why, wasn't this on CONTROLLER?

--- src/RestockerTaskListScreen.java
+++ src/RestockerTaskListScreen.java
@@ -117,9 +117,6 @@ public class RestockerTaskListScreen {
 						+ items.getProduct().getName() 
 						+ " to location " + i + ", " + j), false) );
 				}
-				else if ( items.getId() == nextItems.getId() ) {
-					continue; // Manager didn't update the row and other conditions are fine
-				}
 				else if ( !items.getProduct().equals( 
 					nextItems.getProduct() ) ) {
 					// next products not the same	
