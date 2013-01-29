1da1968a7406f8c35f7fadb8e9cb4097983d3015
Sol Boucher <slb1566@rit.edu>
2013-01-29 01:54:50 -0500

    Added list of items on ManagerAlterLayoutScreenGUI

--- src/ManagerAlterLayoutScreenGUI.java
+++ src/ManagerAlterLayoutScreenGUI.java
@@ -2,10 +2,13 @@ import javax.swing.JPanel;
 import javax.swing.BoxLayout;
 import javax.swing.Box;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JButton;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  * Content panel for the manger's alter layout screen.
@@ -29,9 +32,14 @@ public class ManagerAlterLayoutScreenGUI extends JPanel implements ActionListene
 	private BaseGUI master;
 
 	/**
+	 * The list of available items.
+	 */
+	private JList stockableItems;
+
+	/**
 	 * The button that allows the manager to change row.
 	 **/
-	private JButton changeRowButton;
+	private ConditionButton changeRowButton;
 
 	/**
 	 * The button that allows the manager to empty row.
@@ -41,7 +49,7 @@ public class ManagerAlterLayoutScreenGUI extends JPanel implements ActionListene
 	/**
 	 * The button that allows the manager to commit changes.
 	 **/
-	private JButton commitChangesButton;
+	private ConditionButton commitChangesButton;
 
 	/**
 	 * The button that allows the manager to log out.
@@ -60,9 +68,10 @@ public class ManagerAlterLayoutScreenGUI extends JPanel implements ActionListene
 		this.master = master;
 
 		vmButtons = new VMLayoutPanel(controller.listRows(), false); 
+		stockableItems = new JList(controller.listItems().toArray());
 		
 		
-	//	changeRowButton = new JButton("Change Row");
+		changeRowButton = new ConditionButton("Change Row");
 	//	emptyRowButton = new JButton("Empty Row");
 	//	commitChangesButton = new JButton("Commit Changes");
 	//	logoutButton = new JButton("Return to home screen");
@@ -104,12 +113,11 @@ public class ManagerAlterLayoutScreenGUI extends JPanel implements ActionListene
 		bottomPanel.add(Box.createRigidArea(new Dimension(10,0)));
 
 		// Confim Changes button
-		commitChangesButton = new JButton("Confirm Changes");
+		commitChangesButton = new ConditionButton("Confirm Changes");
 		commitChangesButton.addActionListener(this);
 		bottomPanel.add(commitChangesButton);
 
 		
-	//	changeRowButton.addActionListener(this);
 	//	changeRowButton.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
 	//	add(changeRowButton);
 
@@ -120,6 +128,24 @@ public class ManagerAlterLayoutScreenGUI extends JPanel implements ActionListene
 	//	add(emptyRowButton);
 
 	//	add(Box.createRigidArea(new Dimension(0, 20)));
+		
+		//behavior:
+		changeRowButton.addCondition(new ConditionButtonCondition()
+		{
+			@Override
+			public boolean checkCondition()
+			{
+				return !stockableItems.isSelectionEmpty();
+			}
+		});
+		stockableItems.addListSelectionListener(new ListSelectionListener()
+		{
+			@Override
+			public void valueChanged(ListSelectionEvent ignored)
+			{
+				changeRowButton.checkAndSetEnabled();
+			}
+		});
 	}
 
 	/**
