35ec83f9ffd66551b5902b601256493ec589b2a4
Matthew Koontz <mjk3979@rit.edu>
2013-02-10 19:00:54 -0500

    Added comments to ImageLoader.

--- src/ImageLoader.java
+++ src/ImageLoader.java
@@ -2,18 +2,37 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.awt.image.BufferedImage;
 
+/**
+ * Contains static methods used to load images needed for the GUI.
+ * @author Matthew Koontz
+ **/
 public class ImageLoader
 {
+	/**
+	 * Root path for all images.
+	 **/
 	private static final String IMAGE_ROOT = "/";
+
+	/**
+	 * Path to item purchased notification image.
+	 **/
 	private static final String PURCHASE_ITEM = "item_purchase.png";
 
+	/**
+	 * Private constructor because this class should not be instantiated.
+	 **/
 	private ImageLoader() {}
 
-	private static BufferedImage loadImageByName(String name)
+	/**
+	 * Loads an image using the given path.
+	 * @param path The path to the image (not including the root path).
+	 * @return A BufferedImage instance for the given image.
+	 **/
+	private static BufferedImage loadImageByName(String path)
 	{
 		try
 		{
-			return ImageIO.read(ImageLoader.class.getResource(IMAGE_ROOT + name));
+			return ImageIO.read(ImageLoader.class.getResource(IMAGE_ROOT + path));
 		}
 		catch (Exception ex)
 		{
@@ -21,6 +40,9 @@ public class ImageLoader
 		}
 	}
 
+	/**
+	 * Loads the image for the item purchased notification.
+	 **/
 	public static BufferedImage loadItemPurchaseImage()
 	{
 		return loadImageByName(PURCHASE_ITEM);
